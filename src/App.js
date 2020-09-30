f466333ca1694874b2c64e935b5dbc8d61484b35
jaqueline maciel <67654555+jd-maciel@users.noreply.github.com>
2020-09-30 16:44:42 -0300

    commit 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Sitetotal from './componentes/site-total';
+
+import "./global.css"
+
+
+function App() {
+  return (
+    <>
+    <Sitetotal />
+
+
+    <h1>oi</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/site-total/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sitetotal.css';
+
+const Sitetotal = () => (
+    <container id="sitetotal"> </container>
+);
+
+export default Sitetotal;
